4b0b4c718f9fc09085e4fe6cff4c3a31d9282da7
vilasma <vilasma@cybage.com>
2021-04-27 22:37:02 +0530

    1.0.0

---
+++ SDKReleaseOnPods/SDKReleaseOnPods/SDKReleaseOnPods.h
@@ -0,0 +1,18 @@
+//
+//  SDKReleaseOnPods.h
+//  SDKReleaseOnPods
+//
+//  Created by Vilas on 27/04/21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKReleaseOnPods.
+FOUNDATION_EXPORT double SDKReleaseOnPodsVersionNumber;
+
+//! Project version string for SDKReleaseOnPods.
+FOUNDATION_EXPORT const unsigned char SDKReleaseOnPodsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKReleaseOnPods/PublicHeader.h>
+
+
